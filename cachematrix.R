9d34f189c1a9f0f3b65851f4ef73b77297521006
kbuehmann <kathy.buehmann@gmail.com>
2016-05-06 18:51:12 -0700

    makeCacheMatrix.R and cacheSolve.R functions added to skeleton program.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions check if a matrix has already been solved and if
+## so, returns the inverse. If there's not already an inverse, it
+## calculates it.
 
-## Write a short comment describing this function
+## This function takes a matrix and gives it the ability to be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse of a matrix has already been
+## cached, if not it computes it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
